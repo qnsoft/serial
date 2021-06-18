6b8b423a9b0efa88764c6e05acd6514cb69d7b5a
sxh <dfsxh@163.com>
2021-06-18 10:00:56 +0800

    首次提交

---
+++ portlist/portlist.go
@@ -0,0 +1,43 @@
+//
+// Copyright 2014-2020 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+// portlist is a tool to list all the available serial ports.
+// Just run it and it will produce an output like:
+//
+// $ go run portlist.go
+// Port: /dev/cu.Bluetooth-Incoming-Port
+// Port: /dev/cu.usbmodemFD121
+//    USB ID     2341:8053
+//    USB serial FB7B6060504B5952302E314AFF08191A
+//
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go.bug.st/serial/enumerator"
+)
+
+func main() {
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		return
+	}
+	for _, port := range ports {
+		fmt.Printf("Port: %s\n", port.Name)
+		if port.Product != "" {
+			fmt.Printf("   Product Name: %s\n", port.Product)
+		}
+		if port.IsUSB {
+			fmt.Printf("   USB ID      : %s:%s\n", port.VID, port.PID)
+			fmt.Printf("   USB serial  : %s\n", port.SerialNumber)
+		}
+	}
+}
---
+++ serial_linux_test.go
@@ -0,0 +1,64 @@
+//
+// Copyright 2014-2020 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// Testing code idea and fix thanks to @angri
+// https://github.com/bugst/go-serial/pull/42
+//
+
+package serial
+
+import (
+	"context"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func startSocatAndWaitForPort(t *testing.T, ctx context.Context) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, "socat", "-D", "STDIO", "pty,link=/tmp/faketty")
+	r, err := cmd.StderrPipe()
+	require.NoError(t, err)
+	require.NoError(t, cmd.Start())
+	// Let our fake serial port node appear.
+	// socat will write to stderr before starting transfer phase;
+	// we don't really care what, just that it did, because then it's ready.
+	buf := make([]byte, 1024)
+	_, err = r.Read(buf)
+	require.NoError(t, err)
+	return cmd
+}
+
+func TestSerialReadAndCloseConcurrency(t *testing.T) {
+
+	// Run this test with race detector to actually test that
+	// the correct multitasking behaviour is happening.
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	cmd := startSocatAndWaitForPort(t, ctx)
+	go cmd.Wait()
+
+	port, err := Open("/tmp/faketty", &Mode{})
+	require.NoError(t, err)
+	buf := make([]byte, 100)
+	go port.Read(buf)
+	// let port.Read to start
+	time.Sleep(time.Millisecond * 1)
+	port.Close()
+}
+
+func TestDoubleCloseIsNoop(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	cmd := startSocatAndWaitForPort(t, ctx)
+	go cmd.Wait()
+
+	port, err := Open("/tmp/faketty", &Mode{})
+	require.NoError(t, err)
+	require.NoError(t, port.Close())
+	require.NoError(t, port.Close())
+}
